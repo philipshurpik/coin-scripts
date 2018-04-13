47e9bb57ac6c7c709bc0533e64fe9c43af44d296
philipshurpik <philip.shurpik@gmail.com>
2018-04-13 21:15:29 +0300

    added saving + improved rendering

--- renderer.py
+++ renderer.py
@@ -5,10 +5,16 @@ from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 
 
-def render_sticks(ax, quotes, width=1000, colorup='#00FF00', colordown='#FF0000', alpha=0.8):
+def render_sticks(ax, quotes, has_date, base_rect_coef=0.5, width=1000, colorup='#00FF00', colordown='#FF0000', alpha=0.8):
+    index = 0
+    rect_width = (base_rect_coef * 0.5 if len(quotes) < 100 else base_rect_coef) / len(quotes)
     for q in quotes:
-        t, open, high, low, close, volume, buy_trade, sell_trade = q
-        timestamp = dates.date2num(t)
+        if has_date:
+            open, high, low, close = q[1], q[2], q[3], q[4]
+            timestamp = dates.date2num(q[0])
+        else:
+            open, high, low, close = q[0], q[1], q[2], q[3]
+            timestamp = index
 
         if close >= open:
             color = colorup
@@ -27,7 +33,7 @@ def render_sticks(ax, quotes, width=1000, colorup='#00FF00', colordown='#FF0000'
         )
         rect = Rectangle(
             xy=(timestamp, lower),
-            width=0.5/len(quotes),
+            width=rect_width,
             height=height,
             facecolor=color,
             edgecolor=color,
@@ -36,6 +42,7 @@ def render_sticks(ax, quotes, width=1000, colorup='#00FF00', colordown='#FF0000'
         rect.set_linewidth((width * 0.8)/len(quotes))
         ax.add_line(vline)
         ax.add_patch(rect)
+        index += 1
 
 
 def render_trades(ax, timestamps, prices, marker, color):
@@ -45,15 +52,19 @@ def render_trades(ax, timestamps, prices, marker, color):
     ax.scatter(x, y, marker=marker, color=color)
 
 
-def render(values, title, scale=1):
+def render(values, title, scale=1, base_rect_coef=0.5):
+    has_trades = values.shape[1] == 8
+    has_date = has_trades or values.shape[1] == 6
     fig, ax = plt.subplots(figsize=(20*scale, 8*scale))
-    render_sticks(ax, values, width=20*50*scale)
-    timestamps = dates.date2num(values[:, 0])
-    render_trades(plt, timestamps, prices=values[:, 6], marker='^', color='g')
-    render_trades(plt, timestamps, prices=values[:, 7], marker='v', color='r')
+    render_sticks(ax, values, has_date=has_date, base_rect_coef=base_rect_coef, width=20*50*scale)
+    if has_trades:
+        timestamps = dates.date2num(values[:, 0])
+        render_trades(plt, timestamps, prices=values[:, 6], marker='^', color='g')
+        render_trades(plt, timestamps, prices=values[:, 7], marker='v', color='r')
+    if has_date:
+        ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d %H:%M'))
     ax.autoscale_view()
     ax.xaxis.set_major_locator(ticker.MaxNLocator(20))
-    ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m-%d %H:%M'))
     plt.xticks(rotation=40)
     plt.title(title)
     plt.grid()
